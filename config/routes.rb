2d83d91809817cc8ceaaae7afdc332c7e40edbab
Jeanie Ahn <Jeanie.ahn9@gmail.com>
2021-06-01 12:48:12 +0200

    add association to category model

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     # resources :treatments, only [ :new, :create]
-    #   resources :trackers, only [ :index, :new, :create, :edit, :update]
+    resources :trackers, only [ :index, :new, :create, :edit, :update]
     # tracker_index == dashboard
 end
