b4ed5b8bc1a3d05b59f419e52a560d020a51c105
Jishuo Ma <jishuoma@gmail.com>
2021-06-02 15:46:09 +0200

    added all validations and custom validations to categories

--- app/models/tracker.rb
+++ app/models/tracker.rb
@@ -1,4 +1,9 @@
 class Tracker < ApplicationRecord
   belongs_to :treatment
   belongs_to :category
+
+  validates :rating, presence: true, numericality: { in: 0..10 }
+  validates :notes, presence: true
+  validates :photo, presence: true
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -19,16 +19,47 @@ puts "Creating database..."
 user = User.new(full_name: "Molly", email: "molly@nasa.us", password: "123456", age: "24", gender: "female", skin_color: "black", skin_type: "combination")
 user.save!
 
-treatment = Treatment.new(user_id: user.id, skin_condition: 'eczema', start_date: '01.06.2021', end_date: 'hopefully soon', status: 'start')
+treatment = Treatment.new(user_id: user.id, skin_condition: 'eczema', start_date: '01.06.2021', end_date: '31.12.2021', status: 'start')
 treatment.save!
 
-12.times do
-  category = Category.new(
-    title: ['sleep', 'stress', 'workout', 'diet', 'sun', 'humidity', 'temperature', 'pollution', 'medication', 'products', 'supplements', 'treatments'],
-    group: ['skincare', 'environmental', 'lifestyle']
-    )
-  category.save!
-end
+# titles = ['sleep', 'stress', 'workout', 'diet', 'sun', 'humidity', 'temperature', 'pollution', 'medication', 'products', 'supplements', 'treatments']
+# groups = ['skincare', 'environmental', 'lifestyle']
+
+# category = {
+#   skincare: ['medications', 'products', 'supplements', 'treatments']
+#   environmental: ['sun', 'humidity', 'temperature', 'pollution']
+#   lifestyle: ['sleep', 'stress', 'workout', 'diet']
+# }
+
+# Category.create!(group: 'skincare', title: 'medications')
+Category.new(group: 'skincare', title: 'medications').save!
+Category.new(group: 'skincare', title: 'products').save!
+Category.new(group: 'skincare', title: 'supplements').save!
+Category.new(group: 'skincare', title: 'treatments').save!
+
+Category.new(group: 'environmental', title: 'sun').save!
+Category.new(group: 'environmental', title: 'humidity').save!
+Category.new(group: 'environmental', title: 'temperature').save!
+Category.new(group: 'environmental', title: 'pollution').save!
+
+Category.new(group: 'lifestyle', title: 'sleep').save!
+Category.new(group: 'lifestyle', title: 'stress').save!
+Category.new(group: 'lifestyle', title: 'workout').save!
+Category.new(group: 'lifestyle', title: 'diet').save!
+
+# category.each_with_index do |(key, value), index|
+#   title: value
+#   group: key
+# end
+
+
+# 4.times do
+#   category = Category.new(
+#     title: titles.sample,
+#     group: skincare
+#     )
+#   category.save!
+# end
 
 tracker = Tracker.new(category_id: Category.first.id, treatment_id: treatment.id, rating: '3', notes: 'seeing progress', photo: 'https://images.unsplash.com/photo-1595514446083-236b9985a0e2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80')
 tracker.save!
