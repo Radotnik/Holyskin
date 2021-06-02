0564bda30cf4efb50fa28cc2861a005d89f5a9bd
Prabha Pothen <prabha.pothen@gmail.com>
2021-06-02 16:03:03 +0200

    saving changes made so far and corrected user devise

--- app/controllers/treatments_controller.rb
+++ app/controllers/treatments_controller.rb
@@ -4,42 +4,45 @@ class TreatmentsController < ApplicationController
     if params[:query].present?
     else
       puts "No treatments added"
+     end
   end
 
 #   def show
 #     @treatment = treatment_params
 #   end
 
-#   def new
-#     @treatment = Treatment.new
-#   end
+  def new
+    @treatment = Treatment.new
+    authorize(@treatment)
+  end
 
-#   def create
-#     @treatment = Treatment.new(params[:treatment])
-#     @treatment.save
-#     redirect_to treatment_path(@treatment)
-#   end
+  def create
+    @treatment = Treatment.new(treatment_params)
+    authorize(@treatment)
+    @treatment.user = current_user
+    @treatment.save
+    redirect_to treatments_path
+  end
 
-#   def edit
-#     @treatment = treatment_params
-#   end
+  def edit
+    @treatment = treatment_params
+  end
 
-#   def update
-#     @treatment = treatment_params
-#     @treatment.update
-#     redirect_to treatment_path(@treatment)
-#   end
+  def update
+    @treatment = treatment_params
+    @treatment.update
+    redirect_to treatment_path(@treatment)
+  end
 
-#   def destroy
-#     @treatment = treatment_params
-#     @treatment.destroy
-#     redirect_to treatment_path
-#   end
+  def destroy
+    @treatment = treatment_params
+    @treatment.destroy
+    redirect_to treatment_path
+  end
 
-#   private
+  private
 
-#   def treatment_params
-#     params.require(:treatment).permit(:skin_condition, :start_date, :end_date, :status)
-#   end
+  def treatment_params
+    params.require(:treatment).permit(:skin_condition, :start_date, :end_date, :status)
   end
 end
--- app/policies/treatment_policy.rb
+++ app/policies/treatment_policy.rb
@@ -4,4 +4,7 @@ class TreatmentPolicy < ApplicationPolicy
       scope.where(user_id: user.id)
     end
   end
+  def update?
+    record.user == user
+  end
 end
