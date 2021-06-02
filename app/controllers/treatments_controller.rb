ede79068cf48bf32a7ec6c989ed71495a3d4d9c1
Prabha Pothen <prabha.pothen@gmail.com>
2021-06-02 11:15:37 +0200

    saving first draft

--- app/controllers/treatments_controller.rb
+++ app/controllers/treatments_controller.rb
@@ -3,6 +3,10 @@ class TreatmentsController < ApplicationController
     @treatments = Treatment.all
   end
 
+  def show
+    @treatment = Treatment.find(params[:id])
+  end
+
   def new
     @treatment = Treatment.new
   end
@@ -10,7 +14,23 @@ class TreatmentsController < ApplicationController
   def create
     @treatment = Treatment.new(params[:treatment])
     @treatment.save
-    redirect_to_tracker_path(@tracker)
+    redirect_to_treatment_path(@treatment)
+  end
+
+  def edit
+    @treatment = Treatment.find(params[:id])
+  end
+
+  def update
+    @treatment = Treatment.find(params[:id])
+    @treatment.update
+    redirect_to_treatment_path(@treatment)
+  end
+
+  def destroy
+    @treatment = Treatment.find(params[:id])
+    @treatment.destroy
+    redirect_to_treatment_path
   end
 
   private
