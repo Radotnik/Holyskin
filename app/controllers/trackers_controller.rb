bfcc2243f3cef4ca431cd57b67c8a10741e07ff6
Jishuo Ma <jishuoma@gmail.com>
2021-06-07 12:28:21 +0200

    created routes to category show, added link to category show on dashboard

--- app/controllers/trackers_controller.rb
+++ app/controllers/trackers_controller.rb
@@ -4,6 +4,8 @@ class TrackersController < ApplicationController
     # @trackers = Tracker.all
     start_date = params.fetch(:start_date, Date.today).to_date
     @trackers = Tracker.where(created_at: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
+    @progress_category_id = Category.find_by_title("treatments").id
+    @treatment = Treatment.find(params[:treatment_id])
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
     resources :treatments, only: [ :index, :new, :create] do
-      resources :trackers, only: [ :index, :new, :create ]
-        resources :categories, only: [ :index ]
+      resources :trackers, only: [ :index, :new, :create]
+      resources :categories, only: [ :index, :show ]
 
     end
 
