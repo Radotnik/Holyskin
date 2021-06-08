f8ffd445023ea7c11f54c7198ebd0c93d88df2ae
Jishuo Ma <jishuoma@gmail.com>
2021-06-08 15:14:36 +0200

    added blazer gem and blazer auth in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'treatments#index'
+
+  # Blazer authentification config/routes.rb
+  authenticate :user, ->(user) { user.admin? } do
+  mount Blazer::Engine, at: "blazer"
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
     resources :treatments, only: [ :index, :new, :create] do
