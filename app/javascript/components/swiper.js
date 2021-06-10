950165fde7f25bb27d4bebe423d7237a08bfedcc
Jishuo Ma <jishuoma@gmail.com>
2021-06-10 12:40:33 +0200

    added swiper carousel with progress tracker info

--- app/javascript/components/swiper.js
+++ app/javascript/components/swiper.js
@@ -1,6 +1,10 @@
 // import Swiper JS
 import Swiper from "swiper";
-import 'swiper/swiper-bundle.css';
+// core version + navigation, pagination modules:
+import SwiperCore, { Pagination } from 'swiper/core';
+
+// configure Swiper to use modules
+SwiperCore.use([Pagination]);
 
 // // import Swiper styles
 const initSwiper = () => {
@@ -12,7 +16,7 @@ const initSwiper = () => {
 //   });
   new Swiper(".swiper-container", {
     slidesPerView: "auto",
-    spaceBetween: 30,
+    spaceBetween: 0,
     speed: 800,
     pagination: {
       el: ".swiper-pagination",
